1784a881e206ad289266c1bc2dd4b3fc8d972a2e
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-09-13 22:32:07 +0200

    Finished

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
 
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador]; //const criada para otimizar o acesso em outras chamadas!
+  const instrumento = tecla.classList[1]; //o numero  é para chamar a segunda classe do botão (que seria tecla_pom, tecla_clap, etc)
+
+  const idAudio = `#som_${instrumento}`;
+
+  //   console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  contador = contador + 1;
+  //   console.log(contador);
+}
